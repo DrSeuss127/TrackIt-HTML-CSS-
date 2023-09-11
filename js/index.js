a798e55bc8e488a8b9fe6ac934cabf946200fa69
Clint Nathan Calumpad <72256679+DrSeuss127@users.noreply.github.com>
2023-09-11 22:49:13 +0800

    added fields for register and some js

--- js/index.js
+++ js/index.js
@@ -3,3 +3,14 @@ document.getElementById("login").addEventListener("click", function() {
     // Redirect to the second HTML file
     window.location.href = "/html/menu.html";
 });
+
+
+function validation(){
+    var userNames = document.getElementById("username").value;
+    var passwords = document.getElementById("password").value;
+
+    if(userNames.length=="" && passwords.length==""){
+        alert("Please input your username and password.")
+        return false;
+    }
+}
