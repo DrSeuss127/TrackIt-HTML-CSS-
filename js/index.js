312425f02628093477066486163fbfc73ce0412c
Marckus Santiago <marckus.santiago@pcworth.com>
2023-09-11 20:39:48 +0800

    feat: made login page, connected it to menu

---
+++ js/index.js
@@ -0,0 +1,5 @@
+    // JavaScript code to handle button click event
+document.getElementById("login").addEventListener("click", function() {
+    // Redirect to the second HTML file
+    window.location.href = "/html/menu.html";
+});
