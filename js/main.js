1e04b225e22ca22b1811e972f27c381bcfb75ad9
Merckerk <marckussantiago924@gmail.com>
2023-09-16 22:18:01 +0800

    feat: made auth. todo: check if user is existing

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
-// JavaScript code to handle button click event
-document.getElementById("login").addEventListener("click", function() {
-    // Redirect to the second HTML file
-    window.location.href = "https://127.0.0.1/TrackIt-HTML-CSS-/html/menu.html";
+const loginForm = document.forms["loginForm"];
+
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const username = loginForm.elements["username"].value;
+  const password = loginForm.elements["password"].value;
+
+  const userAccounts = JSON.parse(localStorage.getItem("users")) || [];
+
+  const loggedInUser = userAccounts.find((user) => user.username === username && user.password === password);
+
+  if (loggedInUser) {
+    sessionStorage.setItem("loggedInUsername", loggedInUser.username);
+    window.location.href = "/html/track.html";
+  } else {
+    alert("Invalid username or password. Please try again.");
+  }
 });
+
